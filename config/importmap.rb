e327959687093d90e1b4e1b1ac5a99d9c608ef8f
Philippe DIOLLOT <philippediollot@hotmail.fr>
2022-12-25 23:17:48 +0100

    updating ruby v

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,8 +6,6 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
-# pin "chart", to: "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.js"
-# pin "chart", to: "assets/vendors/chart.js/Chart.min.js"
 pin "chart", to: "https://ga.jspm.io/npm:chart.js@2.9.3/dist/Chart.js"
 pin "moment", to: "https://ga.jspm.io/npm:moment@2.29.4/moment.js"
 pin "progressbar", to: "https://ga.jspm.io/npm:progressbar@1.1.0/index.js"
@@ -15,4 +13,12 @@ pin "editions", to: "https://ga.jspm.io/npm:editions@1.3.4/es2015/index.js"
 pin "path", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/path.js"
 pin "process", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/process-production.js"
 pin "util", to: "https://ga.jspm.io/npm:@jspm/core@2.0.0-beta.27/nodelibs/browser/util.js"
-pin "jvectormap", to: "https://ga.jspm.io/npm:jvectormap@2.0.4/jquery-jvectormap.min.js"
+
+
+# pin "jvectormap", to: "https://ga.jspm.io/npm:jvectormap@2.0.4/jquery-jvectormap.min.js"
+# pin "jquery-jvectormap", to: "assets/vendors/jvectormap/jquery-jvectormap.min.js"
+# pin "jquery-jvectormap-world-mill-en", to: "assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js"
+# pin "owl.carousel", to: "https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"
+# pin "off-canvas", to: "assets/js/off-canvas.js"
+# pin "todolist", to: "assets/js/todolist.js"
+# pin "dashboard", to: "assets/js/dashboard.js"
---
+++ vendor/assets/template.js
@@ -0,0 +1,400 @@
+// original file : vendor/assets/js/dashboard.js
+(function($) {
+  'use strict';
+  $.fn.andSelf = function() {
+    return this.addBack.apply(this, arguments);
+  }
+  $(function() {
+    if ($("#currentBalanceCircle").length) {
+      var bar = new ProgressBar.Circle(currentBalanceCircle, {
+        color: '#000',
+        // This has to be the same size as the maximum width to
+        // prevent clipping
+        strokeWidth: 12,
+        trailWidth: 12,
+        trailColor: '#0d0d0d',
+        easing: 'easeInOut',
+        duration: 1400,
+        text: {
+          autoStyleContainer: false
+        },
+        from: { color: '#d53f3a', width: 12 },
+        to: { color: '#d53f3a', width: 12 },
+        // Set default step function for all animate calls
+        step: function(state, circle) {
+          circle.path.setAttribute('stroke', state.color);
+          circle.path.setAttribute('stroke-width', state.width);
+
+          var value = Math.round(circle.value() * 100);
+          circle.setText('');
+
+        }
+      });
+
+      bar.text.style.fontSize = '1.5rem';
+      bar.animate(0.4);  // Number from 0.0 to 1.0
+    }
+    if($('#audience-map').length) {
+      $('#audience-map').vectorMap({
+        map: 'world_mill_en',
+        backgroundColor: 'transparent',
+        panOnDrag: true,
+        focusOn: {
+          x: 0.5,
+          y: 0.5,
+          scale: 1,
+          animate: true
+        },
+        series: {
+          regions: [{
+            scale: ['#3d3c3c', '#f2f2f2'],
+            normalizeFunction: 'polynomial',
+            values: {
+
+              "BZ": 75.00,
+              "US": 56.25,
+              "AU": 15.45,
+              "GB": 25.00,
+              "RO": 10.25,
+              "GE": 33.25
+            }
+          }]
+        }
+      });
+    }
+    if ($("#transaction-history").length) {
+      var areaData = {
+        labels: ["Paypal", "Stripe","Cash"],
+        datasets: [{
+            data: [55, 25, 20],
+            backgroundColor: [
+              "#111111","#00d25b","#ffab00"
+            ]
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        segmentShowStroke: false,
+        cutoutPercentage: 70,
+        elements: {
+          arc: {
+              borderWidth: 0
+          }
+        },      
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        }
+      }
+      var transactionhistoryChartPlugins = {
+        beforeDraw: function(chart) {
+          var width = chart.chart.width,
+              height = chart.chart.height,
+              ctx = chart.chart.ctx;
+      
+          ctx.restore();
+          var fontSize = 1;
+          ctx.font = fontSize + "rem sans-serif";
+          ctx.textAlign = 'left';
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#ffffff";
+      
+          var text = "$1200", 
+              textX = Math.round((width - ctx.measureText(text).width) / 2),
+              textY = height / 2.4;
+      
+          ctx.fillText(text, textX, textY);
+
+          ctx.restore();
+          var fontSize = 0.75;
+          ctx.font = fontSize + "rem sans-serif";
+          ctx.textAlign = 'left';
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#6c7293";
+
+          var texts = "Total", 
+              textsX = Math.round((width - ctx.measureText(text).width) / 1.93),
+              textsY = height / 1.7;
+      
+          ctx.fillText(texts, textsX, textsY);
+          ctx.save();
+        }
+      }
+      var transactionhistoryChartCanvas = $("#transaction-history").get(0).getContext("2d");
+      var transactionhistoryChart = new Chart(transactionhistoryChartCanvas, {
+        type: 'doughnut',
+        data: areaData,
+        options: areaOptions,
+        plugins: transactionhistoryChartPlugins
+      });
+    }
+    if ($("#transaction-history-arabic").length) {
+      var areaData = {
+        labels: ["Paypal", "Stripe","Cash"],
+        datasets: [{
+            data: [55, 25, 20],
+            backgroundColor: [
+              "#111111","#00d25b","#ffab00"
+            ]
+          }
+        ]
+      };
+      var areaOptions = {
+        responsive: true,
+        maintainAspectRatio: true,
+        segmentShowStroke: false,
+        cutoutPercentage: 70,
+        elements: {
+          arc: {
+              borderWidth: 0
+          }
+        },      
+        legend: {
+          display: false
+        },
+        tooltips: {
+          enabled: true
+        }
+      }
+      var transactionhistoryChartPlugins = {
+        beforeDraw: function(chart) {
+          var width = chart.chart.width,
+              height = chart.chart.height,
+              ctx = chart.chart.ctx;
+      
+          ctx.restore();
+          var fontSize = 1;
+          ctx.font = fontSize + "rem sans-serif";
+          ctx.textAlign = 'left';
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#ffffff";
+      
+          var text = "$1200", 
+              textX = Math.round((width - ctx.measureText(text).width) / 2),
+              textY = height / 2.4;
+      
+          ctx.fillText(text, textX, textY);
+
+          ctx.restore();
+          var fontSize = 0.75;
+          ctx.font = fontSize + "rem sans-serif";
+          ctx.textAlign = 'left';
+          ctx.textBaseline = "middle";
+          ctx.fillStyle = "#6c7293";
+
+          var texts = "مجموع", 
+              textsX = Math.round((width - ctx.measureText(text).width) / 1.93),
+              textsY = height / 1.7;
+      
+          ctx.fillText(texts, textsX, textsY);
+          ctx.save();
+        }
+      }
+      var transactionhistoryChartCanvas = $("#transaction-history-arabic").get(0).getContext("2d");
+      var transactionhistoryChart = new Chart(transactionhistoryChartCanvas, {
+        type: 'doughnut',
+        data: areaData,
+        options: areaOptions,
+        plugins: transactionhistoryChartPlugins
+      });
+    }
+    if ($('#owl-carousel-basic').length) {
+      $('#owl-carousel-basic').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        navText: ["<i class='mdi mdi-chevron-left'></i>", "<i class='mdi mdi-chevron-right'></i>"],
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      });
+    }
+    var isrtl = $("body").hasClass("rtl");
+    if ($('#owl-carousel-rtl').length) {
+      $('#owl-carousel-rtl').owlCarousel({
+        loop: true,
+        margin: 10,
+        dots: false,
+        nav: true,
+        rtl: isrtl,
+        autoplay: true,
+        autoplayTimeout: 4500,
+        navText: ["<i class='mdi mdi-chevron-right'></i>", "<i class='mdi mdi-chevron-left'></i>"],
+        responsive: {
+          0: {
+            items: 1
+          },
+          600: {
+            items: 1
+          },
+          1000: {
+            items: 1
+          }
+        }
+      });
+    }
+    });
+})(jQuery);
+// original file : vendor/assets/js/dashboard.js
+
+// vendor/assets/js/todolist.js
+(function($) {
+  'use strict';
+  $(function() {
+    var todoListItem = $('.todo-list');
+    var todoListInput = $('.todo-list-input');
+    $('.todo-list-add-btn').on("click", function(event) {
+      event.preventDefault();
+
+      var item = $(this).prevAll('.todo-list-input').val();
+
+      if (item) {
+        todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox'/>" + item + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
+        todoListInput.val("");
+      }
+
+    });
+
+    todoListItem.on('change', '.checkbox', function() {
+      if ($(this).attr('checked')) {
+        $(this).removeAttr('checked');
+      } else {
+        $(this).attr('checked', 'checked');
+      }
+
+      $(this).closest("li").toggleClass('completed');
+
+    });
+
+    todoListItem.on('click', '.remove', function() {
+      $(this).parent().remove();
+    });
+
+  });
+})(jQuery);
+// vendor/assets/js/todolist.js
+
+
+
+// vendor/assets/js/settings.js
+(function($) {
+  'use strict';
+  $(function() {
+    $(".nav-settings").click(function() {
+      $("#right-sidebar").toggleClass("open");
+    });
+    $(".settings-close").click(function() {
+      $("#right-sidebar,#theme-settings").removeClass("open");
+    });
+
+    $("#settings-trigger").on("click", function() {
+      $("#theme-settings").toggleClass("open");
+    });
+
+
+    //background constants
+    var navbar_classes = "navbar-danger navbar-success navbar-warning navbar-dark navbar-light navbar-primary navbar-info navbar-pink";
+    var sidebar_classes = "sidebar-light sidebar-dark";
+    var $body = $("body");
+
+    //sidebar backgrounds
+    $("#sidebar-default-theme").on("click", function() {
+      $body.removeClass(sidebar_classes);
+      $(".sidebar-bg-options").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $("#sidebar-dark-theme").on("click", function() {
+      $body.removeClass(sidebar_classes);
+      $body.addClass("sidebar-dark");
+      $(".sidebar-bg-options").removeClass("selected");
+      $(this).addClass("selected");
+    });
+
+
+    //Navbar Backgrounds
+    $(".tiles.primary").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-primary");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.success").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-success");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.warning").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-warning");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.danger").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-danger");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.info").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-info");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.dark").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".navbar").addClass("navbar-dark");
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+    $(".tiles.default").on("click", function() {
+      $(".navbar").removeClass(navbar_classes);
+      $(".tiles").removeClass("selected");
+      $(this).addClass("selected");
+    });
+
+    //Horizontal menu in mobile
+    $('[data-toggle="horizontal-menu-toggle"]').on("click", function() {
+      $(".horizontal-menu .bottom-navbar").toggleClass("header-toggled");
+    });
+    // Horizontal menu navigation in mobile menu on click
+    var navItemClicked = $('.horizontal-menu .page-navigation >.nav-item');
+    navItemClicked.on("click", function(event) {
+      if(window.matchMedia('(max-width: 991px)').matches) {
+        if(!($(this).hasClass('show-submenu'))) {
+          navItemClicked.removeClass('show-submenu');
+        }
+        $(this).toggleClass('show-submenu');
+      }        
+    });
+
+    $(window).scroll(function() {
+      if(window.matchMedia('(min-width: 992px)').matches) {
+        var header = $('.horizontal-menu');
+        if ($(window).scrollTop() >= 71) {
+          $(header).addClass('fixed-on-scroll');
+        } else {
+          $(header).removeClass('fixed-on-scroll');
+        }
+      }
+    });
+
+  });
+})(jQuery);
+// vendor/assets/js/settings.js
