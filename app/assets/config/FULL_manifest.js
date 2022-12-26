820d93c192bf39c2d6198b180398d76c54b39258
Philippe DIOLLOT <philippediollot@hotmail.fr>
2022-12-26 11:44:32 +0100

    changing vendor assets folder name

---
+++ app/assets/config/FULL_manifest.js
@@ -0,0 +1,10 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
+//= link_tree ../../../vendor/assets/vendors .js
+//= link_tree ../../../vendor/assets/js .js
+//= link_tree ../../../vendor/assets .js
+//= link_tree ../../../vendor/assets/images .png
+//= link_tree ../../../vendor/assets/images .jpg
+//= link_tree ../../../vendor/assets/images .svg
